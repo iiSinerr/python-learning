04e0c648b5ee091f2fa19a3aca18e2b49c040fd9
iiSinerr <siner40400@gmail.com>
2025-11-26 13:17:38 +0100

    day1 learning

---
+++ day1.py
@@ -0,0 +1,18 @@
+# print("my first code in this course")
+# print("hello \
+# world")
+#this is comment 
+
+x = 21
+y = 11
+print(x)
+print(y)
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+dala3 = "triyoli"
+print(dala3)
+z = x + y 
+print(z)
