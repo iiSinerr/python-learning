365cf17fb24cc02dfc9b1f2c8aae0916937d89d5
iiSinerr <siner40400@gmail.com>
2025-11-27 11:57:57 +0100

    learned loops and if else

---
+++ day2.py
@@ -0,0 +1,19 @@
+password = 123
+
+x = 120
+
+if password == x:
+    print("correct")
+else:
+    print("wrong") 
+
+
+
+for z in range(0,5,2):
+    print("hey me")
+
+while x <= 122:
+    print("true")
+    x += 1
+else:
+    print("false")
