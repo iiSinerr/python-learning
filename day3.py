aeb1f10bde4808a19813aa93fe465c9ec411270a
iiSinerr <siner40400@gmail.com>
2025-11-30 20:06:46 +0100

    ak chayf

---
+++ day3.py
@@ -0,0 +1,15 @@
+z = 1
+y = 3
+while y > 2:
+
+  password = input("enter your password")
+
+  x = "nazim123"
+
+  if x == password:
+     print("welcome")
+     y = z
+     
+  else:
+     print("wrong password")
+    
